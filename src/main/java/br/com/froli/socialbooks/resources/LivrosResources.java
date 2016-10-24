3cf01fe6f01f79d1378cc3235e2eaf9bb871b11e
Francisco Oliveira <delley.fx@gmail.com>
2016-10-24 20:55:02 -0200

    Add save book

--- src/main/java/br/com/froli/socialbooks/resources/LivrosResources.java
+++ src/main/java/br/com/froli/socialbooks/resources/LivrosResources.java
@@ -3,6 +3,7 @@ package br.com.froli.socialbooks.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,19 @@ import br.com.froli.socialbooks.domain.Livro;
 import br.com.froli.socialbooks.repository.LivrosRepository;
 
 @RestController
+@RequestMapping(value = "/livros")
 public class LivrosResources {
 	
 	@Autowired
 	private LivrosRepository livrosRepository;
 
-	@RequestMapping(value = "/livros", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public List<Livro> listar() {
 		return livrosRepository.findAll();
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public void salvar(@RequestBody Livro livro) {
+		livrosRepository.save(livro);
 	}
 }
