cca09221424eeccc9086c498279efb0970cc8e5e
Francisc Oliveira <delley.fx@gmail.com>
2016-10-30 18:50:34 -0200

    Fixing the formatting of type date fields

--- src/main/java/br/com/froli/socialbooks/handler/ResourceExceptionHandler.java
+++ src/main/java/br/com/froli/socialbooks/handler/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package br.com.froli.socialbooks.handler;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -49,5 +50,17 @@ public class ResourceExceptionHandler {
 		erro.setTimestamp(System.currentTimeMillis());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<DetalhesErro> handlerDataIntegrityViolationException(DataIntegrityViolationException e, HttpServletRequest request) {
+		
+		DetalhesErro erro = new DetalhesErro();
+		erro.setStatus(400L);
+		erro.setTitulo("Requisição inválida");
+		erro.setMensagemDesenvolvedor("https://erro.socialbook.com/400");
+		erro.setTimestamp(System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
 	}
 }
